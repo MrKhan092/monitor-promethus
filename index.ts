d8947f859c6b69157820fc38dba355c5f6c9a9b9
Mohammad Kaif <kaifm0198@gmail.com>
2026-08-30 02:54:32 +0530

    added histogram

--- index.ts
+++ index.ts
@@ -11,12 +11,24 @@ const activeRequestsGauge = new promClinet.Gauge({
     name: 'active_requests',
     help: 'Number of active requests'
 });
+ const httpRequestDurationMicroseconds = new promClinet.Histogram({
+    name: 'http_request_duration_ms',
+    help: 'Duration of HTTP requests in ms',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [0.1, 5, 15, 50, 100, 300, 500, 1000, 3000, 5000] // Define your own buckets here
+});
+
 function middleware(req: Request, res: Response, next: NextFunction) {
     activeRequestsGauge.inc();
     const startTime = Date.now();
 
     res.on('finish', () => {
         const endTime = Date.now();
+        httpRequestDurationMicroseconds.observe({
+            method: req.method,
+            route: req.route ? req.route.path : req.path,
+            code: res.statusCode
+        }, endTime - startTime);
         console.log(`Request took ${endTime - startTime}ms`);
 
         // Increment request counter
