e8e16851a201c23b9466c41e0f1739781d29db98
Mohammad Kaif <kaifm0198@gmail.com>
2026-09-01 19:59:09 +0530

    added the grafana

--- index.ts
+++ index.ts
@@ -1,70 +1,35 @@
 import express from "express";
-import type { Request,Response ,NextFunction} from "express";
-import promClinet from "prom-client";
+import { middleware } from "./middleware";
+import client from "prom-client";
+import { metricsMiddleware } from "./metrics";
 
-const requestCounter = new promClinet.Counter({
-    name: 'http_requests_total',
-    help: 'Total number of HTTP requests',
-    labelNames: ['method', 'route', 'status_code']
-});
-const activeRequestsGauge = new promClinet.Gauge({
-    name: 'active_requests',
-    help: 'Number of active requests'
-});
- const httpRequestDurationMicroseconds = new promClinet.Histogram({
-    name: 'http_request_duration_ms',
-    help: 'Duration of HTTP requests in ms',
-    labelNames: ['method', 'route', 'code'],
-    buckets: [0.1, 5, 15, 50, 100, 300, 500, 1000, 3000, 5000] // Define your own buckets here
-});
+const app = express();
 
-function middleware(req: Request, res: Response, next: NextFunction) {
-    activeRequestsGauge.inc();
-    const startTime = Date.now();
-
-    res.on('finish', () => {
-        const endTime = Date.now();
-        httpRequestDurationMicroseconds.observe({
-            method: req.method,
-            route: req.route ? req.route.path : req.path,
-            code: res.statusCode
-        }, endTime - startTime);
-        console.log(`Request took ${endTime - startTime}ms`);
-
-        // Increment request counter
-        requestCounter.inc({
-            method: req.method,
-            route: req.route ? req.route.path : req.path,
-            status_code: res.statusCode
-        });
-         activeRequestsGauge.dec();
-    });
-   
-
-    next();
-};
-
-const app=express();
+app.use(express.json());
 app.use(middleware);
-app.get('/cpu',(req,res)=>{
-    for( let i=0;i<10000;i++){
-        Math.random();
-    }
-    res.json({
-        message:"CPU intensive task completed"
-    })
+app.use(metricsMiddleware);
+
+app.get("/user", async (req, res) => {
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    res.send({
+        name: "John Doe",
+        age: 25,
+    });
 });
-app.get('/users',(req,res)=>{
-    res.json({
-        message:"users"
-    })
+
+app.post("/user", (req, res) => {
+    const user = req.body;
+    res.send({
+        ...user,
+        id: 1,
+    });
 });
-app.get('/metrics', async (req, res) => {
-    const metrics = await promClinet.register.metrics();
-    console.log(promClinet.register.contentType);
-    res.set('Content-Type', promClinet.register.contentType);
-    res.end(await promClinet.register.metrics());
-});
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-}); 
+
+app.get("/metrics", async (req, res) => {
+    const metrics = await client.register.metrics();
+    res.set('Content-Type', client.register.contentType);
+    res.end(metrics);
+})
+
+app.listen(3000);
